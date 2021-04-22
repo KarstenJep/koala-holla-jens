4ef5ba3f7f1b1914148a94712c136d639f3a6f85
Maneena Xiong <xiong.maneena@gmail.com>
2021-04-22 15:27:55 -0500

    added comments

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,12 +12,13 @@ $( document ).ready( function(){
 function setupClickListeners() {
   $( '#addButton' ).on( 'click', function(){
     console.log( 'in addButton on click' );
+    //called the readyTranser button function 
     $(`#viewKoalas`).on('click', "readyTransfer",  putOnTransfer);
     // get user input and put in an object
     // NOT WORKING YET :(
     // using a test object
     let koalaToSend = {
-      name: $(`#nameIn`).val(),
+      name: $(`#nameIn`).val(),// set objects to value
       age: $(`#ageIn`).val(),
       gender: $(`#genderIn`).val(),
       readyForTransfer: $(`#readyForTransferIn`).val(),
@@ -31,13 +32,14 @@ function setupClickListeners() {
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  $(`#viewKoalas`).empty();
+  $(`#viewKoalas`).empty();// set table to empty 
    $.ajax({
      type: 'GET',
      url: '/koala'
    })
    .then(function (response){
      console.log('Got it!', response);
+     //created a loop to get new inputs from client
      for (let i = 0; i < response.length; i++){
        let newKoal = $(`
        <tr>
@@ -52,7 +54,7 @@ function getKoalas(){
        </tr>
       `)
       newKoal.data('id', response[i].id);
-      $(`#viewKoalas`).append(newKoal);
+      $(`#viewKoalas`).append(newKoal);// appending to DOM
      } 
    });
 } // end getKoalas
@@ -61,31 +63,31 @@ function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
  $.ajax({
-   type: 'POST',
+   type: 'POST',//to post on the DOM
    url: '/koala',
    data: newKoala
   })
   .then(function (response){
-      $(`#nameIn`).val(''),
+      $(`#nameIn`).val(''),//set objects to empty
       $(`#ageIn`).val(''),
       $(`#genderIn`).val(''),
       $(`#readyForTransferIn`).val(''),
       $(`#notesIn`).val('')
-      getKoalas();
+      getKoalas();//to get the koalas from client
   });
 }
 
-function putOnTransfer(){
+function putOnTransfer(){//to call the transfer function when button is pushed
   let id = (readyTransfer($(this).on("id")))
   readyTransfer(id);
 }
 
-function readyTransfer(koalaId){
+function readyTransfer(koalaId){//a function to allow button to be pushed to transfer
   $.ajax({
     method: 'PUT',
     url: `/transfer/${koalaId}`,
     data: {
-      direction: koalaId// should i make an if statment and what goes here.
+      direction: koalaId
     }
     .then(response => {
       getKoalas();
